53ed575580b2221e9ea6c95c5bf547fc6d56083e
ayoubissiakhem <ayoubissiakhem@polymtl.ca>
2024-02-11 16:45:06 -0500

    ajout de methodes, modif display message et 15 derniers messages

--- TP1/src/app/ClientHandler.java
+++ TP1/src/app/ClientHandler.java
@@ -7,6 +7,8 @@ import java.net.Socket;
 import java.util.Calendar;
 import java.util.Deque;
 import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
 
 public class ClientHandler extends Thread { // pour traiter la demande de chaque client sur un socket particulier
 	private static final String USERSFILENAME = "usersDataBase.csv";
@@ -47,34 +49,8 @@ public class ClientHandler extends Thread { // pour traiter la demande de chaque
 			if(entry.equals(existant)){
 				try {
 					out.writeUTF("Hello from server - you are client#" + clientNumber); // envoi de message
-					for (String message : messagesList) {
-						out.writeUTF(message);
-					}
-
-					boolean connect = true;
-					while(connect) { // Attente de réception des messages
-						String message = in.readUTF();
-						if(message.equals("/disconnect")) {
-							connect = false;
-						}
-						else if(message.length()>200){
-							out.writeUTF("Attention: La taille des messages est limitée à 200 caractères. Le message n'a pas été envoyé");
-						}
-						else {
-							// Print côté clients
-							printToEachClient("["+entry.getUsername() + "]:" + message);
-							// Print côté serveur
-							Calendar cal = Calendar.getInstance();
-							message = "[" + entry.getUsername() + " - " + 
-									socket.getInetAddress().getHostAddress() + ":" + socket.getPort() + " - "+
-									cal.get(Calendar.DAY_OF_MONTH)+cal.get(Calendar.MONTH)+cal.get(Calendar.YEAR)+
-									cal.get(Calendar.HOUR)+cal.get(Calendar.MINUTE)+cal.get(Calendar.SECOND)+"]: " + 
-									message;
-							System.out.println(message);
-							messagesList.add(message);
-							Utils.writeToDatabase(message, MESSAGESFILENAME);
-						}
-					}
+					displayOldMessages(out);
+					serverClientInteraction(out, in, entry);
 				} catch (IOException e) {
 					System.out.println("Error handling client# " + clientNumber + ": " + e);
 				} finally {
@@ -94,6 +70,49 @@ public class ClientHandler extends Thread { // pour traiter la demande de chaque
 		} catch (IOException e) {
 			System.out.println("Erreur de lecture des données du client: " + e.getMessage());
 		}
+	}
+	
+	private void serverClientInteraction(DataOutputStream out, DataInputStream in, Utilisateur entry) throws IOException {
+		boolean connect = true;
+		while(connect) { 
+			String message = in.readUTF();
+			if(message.equals("/disconnect")) {
+				connect = false;
+			}
+			else if(message.length()>200){
+				out.writeUTF("Attention: La taille des messages est limitée à 200 caractères. Le message n'a pas été envoyé");
+			}
+			else {
+				displayMessage(entry, message);
+			}
+		}
+	}
+	
+	private void displayOldMessages(DataOutputStream out) throws IOException {
+		List<String> lastMessagesList = new ArrayList<>();
+		int count = 0;
+		for (Iterator<String> iterator = messagesList.descendingIterator(); iterator.hasNext() && count < 15; ) {
+		    String message = iterator.next();
+		    lastMessagesList.add(message);
+		    //out.writeUTF(message);
+		    count++;
+		}
+		for (int i = lastMessagesList.size() - 1; i >= 0; i--) {
+			out.writeUTF(lastMessagesList.get(i));
+		}
+	}
+	
+	private void displayMessage(Utilisateur entry, String message) {
+		printToEachClient("["+entry.getUsername() + "]:" + message);
+		Calendar cal = Calendar.getInstance();
+		message = "[" + entry.getUsername() + " - " + 
+				socket.getInetAddress().getHostAddress() + ":" + socket.getPort() + " - "+
+				cal.get(Calendar.YEAR)+ "-" +cal.get(Calendar.MONTH)+ "-" +cal.get(Calendar.DAY_OF_MONTH)+ "@" +
+				cal.get(Calendar.HOUR)+ ":" +cal.get(Calendar.MINUTE)+ ":" +cal.get(Calendar.SECOND)+"]: " + 
+				message;
+		System.out.println(message);
+		messagesList.add(message);
+		Utils.writeToDatabase(message, MESSAGESFILENAME);
 	}
 
 	private void printToEachClient(String message) {
